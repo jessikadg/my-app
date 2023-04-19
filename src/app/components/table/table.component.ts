8b6eea1eb82a28f3a251ee6370721724fcc91856
Jessika <jessikadg@gmail.com>
2023-04-19 14:42:09 +0200

    Data imported and passed to Table successfully

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DataService } from 'src/app/data-service/data.service';
-import { Users } from 'src/app/type-definitions/User.model';
-import * as userData from '../../_files/users.json';
+import { User } from 'src/app/type-definitions/User.model';
+import users from '../../_files/users.json';
 
 @Component({
   selector: 'app-table',
@@ -9,13 +9,23 @@ import * as userData from '../../_files/users.json';
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent {
-  public userList: any = userData;
-  users = Users;
+  // type should be Observable<> if it comes from a service.
+  public userList: User[] = users.users;
+
+  usersForTable: User[] = [];
+  // users = User[];
+
+  public usersHardCoded: User[] = [
+    // new User(1, 'John Test', 'Display John', 'email@email.com', 'description'),
+    // this.userList.map(())
+  ];
 
   constructor(public DataService: DataService) {}
 
   ngOnInit() {
-    console.log(this.userList.users);
-    this.users = this.userList.users;
+    console.log(this.userList);
+    this.usersForTable = this.userList;
+    this.usersHardCoded = this.usersHardCoded;
+    // console.log(this.usersHardCoded);
   }
 }
--- src/app/type-definitions/User.model.ts
+++ src/app/type-definitions/User.model.ts
@@ -1,16 +1,21 @@
-import { EmailValidator } from '@angular/forms';
-
-export class Users {
+export class User {
   public id: number;
   public fullName: string;
+  public displayName: string;
   public email: string;
   public details: string;
 
-  constructor(id: number, fullName: string, email: string, details: string) {
+  constructor(
+    id: number,
+    fullName: string,
+    email: string,
+    details: string,
+    displayName: string
+  ) {
     this.id = id;
     this.fullName = fullName;
     this.email = email;
     this.details = details;
+    this.displayName = displayName;
   }
 }
-[];
