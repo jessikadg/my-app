e34e12d3df2d9822ee38712ddbc4074524bb9959
Jessika <jessikadg@gmail.com>
2023-04-19 20:01:58 +0200

    Forms inserted to update User Data
    No connection to server yet, no styling. Working forms.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
 import { DetailsComponent } from './components/details/details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, TableComponent, DetailsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { DataService } from 'src/app/data-service/data.service';
 import { User } from 'src/app/type-definitions/User.model';
@@ -22,7 +23,18 @@ export class DetailsComponent {
     details: '',
   };
 
-  constructor(public DataService: DataService, private route: ActivatedRoute) {}
+  constructor(
+    public DataService: DataService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
+
+  updateUserForm = this.formBuilder.group({
+    fullName: '',
+    email: '',
+    displayName: '',
+    details: '',
+  });
 
   ngOnInit() {
     this.usersOnDetails = this.DataService.userList;
@@ -33,5 +45,26 @@ export class DetailsComponent {
     });
 
     this.selectedUser = this.DataService.getUserById(this.id);
+
+    //Pre-filling the Form with Data from the Service:
+    this.updateUserForm
+      .get('fullName')
+      ?.setValue(this.selectedUser?.fullName || 'Name');
+
+    this.updateUserForm
+      .get('email')
+      ?.setValue(this.selectedUser?.email || 'Email');
+
+    this.updateUserForm
+      .get('displayName')
+      ?.setValue(this.selectedUser?.displayName || 'Display Name');
+
+    this.updateUserForm
+      .get('details')
+      ?.setValue(this.selectedUser?.details || 'Details');
+  }
+
+  onSubmit(): void {
+    console.log(this.updateUserForm.value);
   }
 }
