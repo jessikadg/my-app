944aef0e7bc1ec3ab6ef5693a0a28cc3ea6f8e97
Jessika <jessikadg@gmail.com>
2023-04-19 16:30:30 +0200

    Displaying Details with Service

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/data-service/data.service';
+import { User } from 'src/app/type-definitions/User.model';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent {}
+export class DetailsComponent {
+  usersOnDetails: User[] = [];
+  constructor(public DataService: DataService) {}
+
+  ngOnInit() {
+    console.log(this.DataService.userList);
+    this.usersOnDetails = this.DataService.userList;
+  }
+}
