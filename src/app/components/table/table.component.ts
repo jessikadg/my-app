c0ccbfced32a868274dc85055ab9232fac24f327
Jessika <jessikadg@gmail.com>
2023-04-19 15:10:37 +0200

    SortById and SortByName handled

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -9,23 +9,23 @@ import users from '../../_files/users.json';
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent {
-  // type should be Observable<> if it comes from a service.
   public userList: User[] = users.users;
 
   usersForTable: User[] = [];
-  // users = User[];
-
-  public usersHardCoded: User[] = [
-    // new User(1, 'John Test', 'Display John', 'email@email.com', 'description'),
-    // this.userList.map(())
-  ];
 
   constructor(public DataService: DataService) {}
 
   ngOnInit() {
     console.log(this.userList);
     this.usersForTable = this.userList;
-    this.usersHardCoded = this.usersHardCoded;
-    // console.log(this.usersHardCoded);
+  }
+
+  onSortById() {
+    this.usersForTable.sort((a, b) => a.id - b.id);
+    console.log(this.usersForTable.sort((a, b) => a.id - b.id));
+  }
+
+  onSortByName() {
+    this.usersForTable.sort((a, b) => a.fullName.localeCompare(b.fullName));
   }
 }
